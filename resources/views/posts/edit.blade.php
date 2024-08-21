e9d40a17a061a23b53da6b174c7b333164b10809
MaiTakada0223 <iamhappy0223@gmail.com>
2024-08-21 15:47:54 +0800

    larablogからコピペしました

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,12 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('layouts.app_original')
+@section('content')
+
+
+  <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <form action="{{ route('posts.update', $post->id) }}" method="POST">
+              @csrf
+              @method('put')
+                <div class="form-group">
+                    <label>タイトル</label>
+                    <input type="text" class="form-control" value="{{ $post->title }}" name="title">
+                </div>
+                <div class="form-group">
+                    <label>内容</label>
+                    <textarea class="form-control" rows="5" name="body">{{ $post->body }}</textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">更新する</button>
+            </form>
+        </div>
+    </div>
+  </div>
+ 
+  @endsection
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,12 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('layouts.app_original')
+@section('content') 
+
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+             <div class="card text-center">
+            <div class="card-header">
+                投稿一覧
+            </div>
+            @foreach($posts as $post)
+            <div class="card-body">
+                <h5 class="card-title">タイトル : {{ $post->title }}</h5>
+                <p class="card-text">
+                  内容 : {{ $post->body }}
+                </p>
+                <p class="card-text">投稿者：{{ $post->user->name }}</p>
+                <a href="{{ route('posts.show', $post->id) }}" class="btn btn-primary">詳細へ</a>
+            </div>
+            <div class="card-footer text-muted">
+                投稿日時 : {{ $post->created_at }}
+            </div>
+            @endforeach
+        </div>
+        </div>
+        <div class="col-md-2">
+          <a href="{{ route('posts.create') }}" class="btn btn-primary">
+            新規投稿
+          </a>
+        </div>
+    </div>
+  </div>
+  @endsection
