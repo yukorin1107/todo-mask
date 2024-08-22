9d8a68893f565d7e17f268aa6d134a424d6258a1
yukorin1107 <yuko.w.1107@gmail.com>
2024-08-22 13:12:00 +0800

    use authを足しました

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
