ef1d27353f5fab209e3d23f3709b25c453449f9b
yukorin1107 <yuko.w.1107@gmail.com>
2024-08-26 16:44:58 +0800

    アコーディオンとモーダルウィンドウ

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -11,12 +11,56 @@
                   <div class="card-body">
                   <p class="card-text">内容：{{ $task->description }}</p>
                   <p>投稿日時：{{ $task->created_at }}</p>
-                  <a href="{{ route('posts.edit',$task->id) }}" class="btn btn-primary">編集する</a>
+                  {{-- <a href="{{ route('posts.edit',$task->id) }}" class="btn btn-primary">編集する</a> --}}
+
+                  <!-- Button trigger modal -->
+                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Edit
+                  </button>
+
+                  <!-- Modal -->
+                  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                          <div class="container">
+                            <div class="row justify-content-center">
+                                <div class="col-md-8">
+                                  @if (Auth::user()->id == $task->user_id)
+                                    <form action="{{ route('posts.update', $task->id) }}" method="POST">
+                                      @csrf
+                                      @method('put')
+                                        <div class="form-group">
+                                            <label>タイトル</label>
+                                            <input type="text" class="form-control" value="{{ $task->name }}" name="name">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>内容</label>
+                                            <textarea class="form-control" rows="5" name="description">{{ $task->description }}</textarea>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary">更新する</button>
+                                    </form>
+                                    @endif
+                                </div>
+                            </div>
+                          </div>
+                        </div>
+                        {{-- <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                          <button type="button" class="btn btn-primary">Save changes</button>
+                        </div> --}}
+                      </div>
+                    </div>
+                  </div>
                   @if (Auth::user()->id == $task->user_id)
                   <form action='{{ route('posts.destroy',$task->id) }}' method='post'>
                     @csrf
                     @method('delete')
-                      <input type='submit' value='削除' class="btn btn-danger" onclick='return confirm("本当に削除しますか？");'>
+                      <input type='submit' value='Delete' class="btn btn-danger" onclick='return confirm("本当に削除しますか？");'>
                   </form>
                   @endif
                   </div>
