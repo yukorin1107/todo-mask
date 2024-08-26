40f9f5c56df8a9d549579ad3ee85381fd9ec3b38
Gebe-Captain <hokuto0128.21@gmail.com>
2024-08-26 18:22:34 +0900

    完了一覧画面にもアコーディオンを採用しました

--- resources/views/Complete/index.blade.php
+++ resources/views/Complete/index.blade.php
@@ -44,7 +44,7 @@
     </div>
 @endsection --}}
 
-<style>
+{{-- <style>
     .thumbnail {
         width: 100px;
         height: 100px;
@@ -66,9 +66,16 @@
                         完了タスク一覧
                     </div>
                     <div class="card-body">
-                        @foreach(['Reading', 'Listening', 'Speaking', 'Writing', 'Vocabulary'] as $type)
-                            <h3>{{ $type }}</h3>
-                            <hr>
+                        <div class="accordion" id="accordionExample">
+                            @foreach(['Reading', 'Listening', 'Speaking', 'Writing', 'Vocabulary'] as $index => $type)
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="heading{{ $index }}" >
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{ $index }}" aria-expanded="false" aria-controls="collapse{{ $index }}">
+                                        {{ $type }}
+                                    </button>
+                                </h2>
+                                <div id="collapse{{ $index }}" class="accordion-collapse collapse" aria-labelledby="heading{{ $index }}" data-bs-parent="#accordionExample"> 
+                                    <div class="accordion-body">
                             @foreach($completedTasks->where('type', $type) as $task)
                                 <div class="task-item mb-4">
                                     <h5 class="card-title">タイトル : {{ $task->name }}</h5>
@@ -92,5 +99,66 @@
             </div>
         </div>
     </div>
+@endsection --}}
+
+<style>
+    .thumbnail {
+        width: 100px;
+        height: 100px;
+    }
+</style>
+
+@extends('layouts.app')
+
+@section('content') 
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <h2>完了画面</h2>
+                <h1 class="display-3 text-white bg-dark font-weight-bolder text-uppercase text-center p-4 shadow-lg">
+                    よくぞ試練を乗り越えた。<br>お主はもっと強くなれる。
+                </h1>
+                <div class="card text-center">
+                    <div class="card-header">
+                        完了タスク一覧
+                    </div>
+                    <div class="card-body">
+                        <div class="accordion" id="accordionExample">
+                            @foreach(['Reading', 'Listening', 'Speaking', 'Writing', 'Vocabulary'] as $index => $type)
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="heading{{ $index }}">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{ $index }}" aria-expanded="false" aria-controls="collapse{{ $index }}">
+                                            {{ $type }}
+                                        </button>
+                                    </h2>
+                                    <div id="collapse{{ $index }}" class="accordion-collapse collapse" aria-labelledby="heading{{ $index }}" data-bs-parent="#accordionExample">
+                                        <div class="accordion-body">
+                                            @foreach($completedTasks->where('type', $type) as $task)
+                                                <div class="task-item mb-4">
+                                                    <h5 class="card-title">タイトル : {{ $task->name }}</h5>
+                                                    <p class="card-text">内容 : {{ $task->description }}</p>
+
+                                                    @if($task->image)
+                                                        <div class="image mt-3">
+                                                            <img src="{{ Storage::url('images/' . $task->image) }}" alt="" class="thumbnail">
+                                                        </div>
+                                                    @endif
+
+                                                    <div class="card-footer text-muted">
+                                                        完了日時 : {{ $task->updated_at }} 
+                                                    </div>
+                                                </div>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
+
 
