2fd35937cd6d2d2607518e00b2e98b27570164df
Gebe-Captain <hokuto0128.21@gmail.com>
2024-08-26 18:31:23 +0900

    目標入力画面の作成

--- resources/views/goals/create.blade.php
+++ resources/views/goals/create.blade.php
@@ -1,17 +1,28 @@
 @extends('layouts.app')
 @section('content') 
 
-<div>
-    <h1>目標を入力しよう!</h1>
-</div>
-<div>
-    <form action="{{ route('goals.store') }}" method="POST">
-        @csrf
-        <div>
-            <label>目標</label>
-            <input type="text" placeholder="目標を記入してください" name="goal_body">
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header text-center bg-primary text-white">
+                    <h2>目標を入力しよう!</h2>
+                </div>
+                <div class="card-body">
+                    <form action="{{ route('goals.store') }}" method="POST">
+                        @csrf
+                        <div class="form-group mb-3">
+                            <label for="goal_body" class="form-label">目標</label>
+                            <input type="text" class="form-control" id="goal_body" name="goal_body" placeholder="目標を記入してください" required>
+                        </div>
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-success">作成</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-        <button type="submit">作成</button>
-    </form>
+    </div>
 </div>
+
 @endsection
