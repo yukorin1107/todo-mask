e39236ef37be5c5822d1dfc480a9f3e8497ade9b
yukorin1107 <yuko.w.1107@gmail.com>
2024-08-27 14:35:56 +0800

    完備修正

--- resources/views/study_time/create.blade.php
+++ resources/views/study_time/create.blade.php
@@ -15,19 +15,10 @@
             <label for="date">日付</label>
             <input type="date" name="date" id="date" class="form-control" required>
         </div>
-<<<<<<< HEAD:resources/views/study_time/create.blade.php
-        <input type="hidden" name="task_id" value="{{ $taskId }}">
-        <button type="submit">OK</button>
-=======
-        <button type="submit">記録する</button>
 
-        {{-- <script>
-            function submitForm() {
-                alert('学習時間が記録されました');
-                document.getElementById('studyTimeForm').submit(); // Submit the form after the alert
-            }
-        </script> --}}
->>>>>>> origin/studytime_in_mypage_mai:resources/views/StudyTime/create.blade.php
+        <input type="hidden" name="task_id" value="{{ $taskId }}">
+
+        <button type="submit">記録する</button>
     </form>
 
     
