6e510d25831ca361cf9ad0fcee5904540db2099c
MaiTakada0223 <iamhappy0223@gmail.com>
2024-08-29 14:49:54 +0800

    cssが完了しました

--- resources/views/mypages/mypage.blade.php
+++ resources/views/mypages/mypage.blade.php
@@ -23,15 +23,15 @@
     <form action="{{ route('profile.image.update') }}" method="POST" enctype="multipart/form-data">
         @csrf
         <div class="form-group">
-            <label for="profile_image">画像変更</label>
+            <label for="profile_image"></label>
             <input type="file" class="form-control-file" id="profile_image" name="profile_image">
-        </div>
         <button type="submit" class="btn btn-primary">更新</button>
     </form>
     <form action="{{ route('profile.image.delete') }}" method="POST" style="display:inline;">
         @csrf
         <button type="submit" class="btn btn-danger">削除</button>
     </form>
+        </div>
     
 
         <div class="username">
@@ -44,21 +44,23 @@
             @foreach($goals as $goal)
                     <p>{{ $goal->goal_body }}</p>
 
-                    <a href="{{ route('goalsprofile.edit', $goal->id) }}" class="btn btn-secondary">Edit</a>
+            <div class="edit">
+                        <a href="{{ route('goalsprofile.edit', $goal->id) }}" class="btn btn-secondary">Edit</a>
+            </div>
             @endforeach
         </div>
+        <br>
         
         <span class="box-title"><img src="{{  asset('img/mypage/goal.png') }}" alt="">学習記録</span>
     <div class=kiroku>
-
         <div class="login-streak">
-        <h2><img src="{{  asset('img/mypage/炎.png') }}" alt=""> {{ $consecutiveLoginDays }}日</h2> 
+        <h2><img src="{{  asset('img/mypage/炎.png') }}" alt=""> {{ $consecutiveLoginDays }}  日</h2> 
 
         <p>連続ログイン履歴</p>
         </div> 
 
         <div class="study-time">
-        <h2><img src="{{  asset('img/mypage/studytime.png') }}" alt="">{{ $StudyTime }} 時間</h2>
+        <h2><img src="{{  asset('img/mypage/studytime.png') }}"  alt="">{{ $StudyTime }} h</h2>
         <p>合計学習時間</p>
         </div>
     </div>
